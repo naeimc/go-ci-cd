4ad02458877bcdc7380f027cea4fc03622f53535
naeimc <naeimc@gmail.com>
2024-01-19 13:35:50 +0000

    added imported package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/naeimc/go-ci-cd
 
 go 1.21.5
+
+require golang.org/x/crypto v0.18.0
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+
+	"golang.org/x/crypto/sha3"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(base64.StdEncoding.EncodeToString(sha3.NewShake256().Sum([]byte{})))
 }
